228ea77ba518c9293f7521b77af25aedf452c603
Smart-Rwl <110030446+Smart-rwl@users.noreply.github.com>
2026-03-23 16:27:50 +0530

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,11 @@
-// lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as {
-  prisma?: PrismaClient;
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient();
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL,
+  })
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
